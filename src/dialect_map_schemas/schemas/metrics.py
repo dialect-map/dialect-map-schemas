4770876f99ff0fdf1c8cf7107007849437282119
Sinclert Pérez <Sinclert@hotmail.com>
2021-06-29 10:10:50 +0200

    Add frequency field validators

--- src/dialect_map_schemas/schemas/metrics.py
+++ src/dialect_map_schemas/schemas/metrics.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 
 from marshmallow import fields
+from marshmallow import validates
 
 from .base import BaseStaticSchema
 
@@ -15,6 +16,25 @@ class JargonCategoryMetrics(BaseStaticSchema):
     rel_freq = fields.Float(required=True)
     created_at = fields.DateTime(required=True)
 
+    @validates("abs_freq")
+    def validate_abs_freq(self, freq: int):
+        """
+        Validates jargon absolute frequency within a category
+        :param freq: jargon term occurrences within a category
+        """
+
+        assert freq >= 0, f"Invalid absolute frequency: {freq}"
+
+    @validates("rel_freq")
+    def validate_rel_freq(self, freq: float):
+        """
+        Validates jargon relative frequency within a category
+        :param freq: jargon term relative frequency within a category
+        """
+
+        assert freq >= 0, f"Invalid relative frequency: {freq}"
+        assert freq <= 1, f"Invalid relative frequency: {freq}"
+
 
 class JargonPaperMetrics(BaseStaticSchema):
     """ArXiv paper jargon NLP metrics"""
@@ -26,3 +46,22 @@ class JargonPaperMetrics(BaseStaticSchema):
     abs_freq = fields.Integer(required=True)
     rel_freq = fields.Float(required=True)
     created_at = fields.DateTime(required=True)
+
+    @validates("abs_freq")
+    def validate_abs_freq(self, freq: int):
+        """
+        Validates jargon absolute frequency within a paper
+        :param freq: jargon term occurrences within a paper
+        """
+
+        assert freq >= 0, f"Invalid absolute frequency: {freq}"
+
+    @validates("rel_freq")
+    def validate_rel_freq(self, freq: float):
+        """
+        Validates jargon relative frequency within a paper
+        :param freq: jargon term relative frequency within a paper
+        """
+
+        assert freq >= 0, f"Invalid relative frequency: {freq}"
+        assert freq <= 1, f"Invalid relative frequency: {freq}"
