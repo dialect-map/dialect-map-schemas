a21c04df9e235a02fdbbb6ef70412f8fcf7a150c
Sinclert Pérez <Sinclert@hotmail.com>
2021-06-30 10:12:08 +0200

    Define routes __init__

---
+++ src/dialect_map_schemas/routes/__init__.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+
+from .api_route import APIRoute
+
+from .dialect_map import DM_CATEGORY_ROUTE
+from .dialect_map import DM_CATEGORY_MEMBER_ROUTE
+from .dialect_map import DM_CATEGORY_METRICS_ROUTE
+
+from .dialect_map import DM_JARGON_ROUTE
+from .dialect_map import DM_JARGON_GROUP_ROUTE
+
+from .dialect_map import DM_PAPER_ROUTE
+from .dialect_map import DM_PAPER_AUTHOR_ROUTE
+from .dialect_map import DM_PAPER_METRICS_ROUTE
+
+from .dialect_map import DM_REFERENCE_ROUTE
+from .dialect_map import DM_REFERENCE_COUNTERS_ROUTE
