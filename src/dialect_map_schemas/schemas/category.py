7f7e93436d3c2e9424f51f3c4870fb1b5aac8684
Sinclert Pérez <Sinclert@hotmail.com>
2021-06-30 17:15:27 +0200

    Define Category alternative keys

--- src/dialect_map_schemas/schemas/category.py
+++ src/dialect_map_schemas/schemas/category.py
@@ -14,8 +14,8 @@ CATEGORY_ID_REGEX = re.compile("^\\w+(-\\w+)?(\\.\\w+)?(-\\w+)?$")
 class Category(BaseArchivalSchema):
     """ArXiv category information record"""
 
-    category_id = fields.String(required=True)
-    description = fields.String(required=True)
+    category_id = fields.String(required=True, metadata={"alt": ["id"]})
+    description = fields.String(required=True, metadata={"alt": ["name"]})
     archived = fields.Boolean(required=True)
     created_at = fields.DateTime(required=True)
     archived_at = fields.DateTime(required=False)
