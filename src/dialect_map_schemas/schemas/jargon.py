90596a4a9e9463155ac951a632f77832b3962e8c
Sinclert Pérez <Sinclert@hotmail.com>
2021-07-09 11:42:42 +0200

    Remove Jargon metadata specs

--- src/dialect_map_schemas/schemas/jargon.py
+++ src/dialect_map_schemas/schemas/jargon.py
@@ -15,9 +15,9 @@ GROUP_ID_REGEX = re.compile("^group-\\d+$")
 class Jargon(BaseArchivalSchema):
     """Jargon term related information record"""
 
-    jargon_id = fields.String(required=True, metadata={"alt": ["id"]})
-    jargon_term = fields.String(required=True, metadata={"alt": ["name"]})
-    jargon_regex = fields.String(required=True, metadata={"alt": ["regex"]})
+    jargon_id = fields.String(required=True)
+    jargon_term = fields.String(required=True)
+    jargon_regex = fields.String(required=True)
     group_id = fields.String(required=False)
     archived = fields.Boolean(required=True)
     created_at = fields.DateTime(required=True)
