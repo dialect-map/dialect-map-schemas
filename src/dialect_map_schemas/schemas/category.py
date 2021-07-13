5964c5f4822fc388216a48ff5468f25759e2271e
Sinclert Pérez <Sinclert@hotmail.com>
2021-07-13 11:42:33 +0200

    Remove category ID validator

--- src/dialect_map_schemas/schemas/category.py
+++ src/dialect_map_schemas/schemas/category.py
@@ -1,14 +1,8 @@
 # -*- coding: utf-8 -*-
 
-import re
-
 from marshmallow import fields
-from marshmallow import validates
 
 from .base import BaseArchivalSchema
-
-
-CATEGORY_ID_REGEX = re.compile("^\\w+(-\\w+)?(\\.\\w+)?(-\\w+)?$")
 
 
 class Category(BaseArchivalSchema):
@@ -28,12 +22,3 @@ class Category(BaseArchivalSchema):
         """
 
         return str(self.category_id.name)
-
-    @validates("category_id")
-    def validate_category_id(self, id: str):
-        """
-        Validates the category ID format
-        :param id: category ID
-        """
-
-        assert re.match(CATEGORY_ID_REGEX, id), f"Invalid ID: {id}"
